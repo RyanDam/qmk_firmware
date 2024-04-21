4d9f649d01e4f9e44acb12b006d0e96a21284e52
ryandam <py.ryan.dam@gmail.com>
2024-04-21 15:53:02 +0700

    Add keymap for all layer

--- keyboards/coban/pad9b/keymaps/default/keymap.c
+++ keyboards/coban/pad9b/keymaps/default/keymap.c
@@ -18,14 +18,32 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        QK_BOOT,
-        KC_MPRV, KC_UP,   KC_MNXT,
+        TO(1),
+        KC_MPRV,   KC_UP,   KC_MNXT,
         KC_LEFT, KC_DOWN, KC_RIGHT
+    ),
+    [1] = LAYOUT(
+        TO(2),
+        KC_1,   KC_2,   KC_3,
+        KC_4,   KC_5,   KC_6
+    ),
+    [2] = LAYOUT(
+        TO(3),
+        KC_A,   KC_B,   KC_C,
+        KC_D,   KC_E,   KC_F
+    ),
+    [3] = LAYOUT(
+        TO(0),
+        QK_BACKLIGHT_DOWN, QK_BACKLIGHT_UP,   QK_BOOT,
+        KC_F1, KC_F2, KC_F3
     ),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [1] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [2] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [3] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
 };
 #endif
